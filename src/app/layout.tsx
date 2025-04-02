2e82dcd6eb917f3d3378b09c04aa60c6c6cb1bb1
Steven Sinakhot <sinakhot.steven@gmail.com>
2025-04-02 00:10:55 +0000

    added nojekyll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,16 +16,9 @@ const geist = Geist({
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Tilozign - Coming Soon</title>
-      </head>
+    <html lang="en" className={`${geist.variable}`}>
       <body>{children}</body>
     </html>
   );
